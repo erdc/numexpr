324306cb8d5bba77221d84e4593b013b948ff696
Gregor.Thalhammer <Gregor.Thalhammer@23631f54-0842-0410-b4f0-d76cd42f001b>
2009-01-26 13:41:42 +0000

    Rewrote setup.py, now uses numpy.distutils. Takes configuration from site.cfg, added site.cfg.example as template. Build information (about MKL) is stored in __config__.py which is created during the build process. This is used in__init__ to detect whether MKL/VML is available. Also changed handling of version numbering. Like in numpy, builds from svn versions are automatically marked as version + 'dev' + svn revision.
    git-svn-id: http://numexpr.googlecode.com/svn/branches/vml_support@51 23631f54-0842-0410-b4f0-d76cd42f001b

--- setup.py
+++ setup.py
@@ -1,84 +1,92 @@
-try:
-    import setuptools
-except ImportError:
-    setuptools = None
-from distutils.core import setup, Extension
-from distutils.command.build_ext import build_ext as old_build_ext
-import os.path
+#!/usr/bin/env python
 
-import numpy
+def configuration():
+    from numpy.distutils.misc_util import Configuration, dict_append
+    from numpy.distutils.system_info import system_info
+    
+    config = Configuration(package_name = 'numexpr')
+    
+    #try to find configuration for MKL, either from environment or site.cfg
+    mkl_config_data = config.get_info('mkl')
+    #some version of MKL need to be linked with libgfortran, for this, use
+    #entries of DEFAULT section in site.cfg
+    default_config = system_info()
+    dict_append(mkl_config_data, 
+                libraries = default_config.get_libraries(),
+                library_dirs = default_config.get_lib_dirs() )
 
-#use_vml = False
-use_vml = True
+    #setup information for C extension
+    extension_config_data = {'sources': ['numexpr/interpreter.c'],
+                             'depends': ['numexpr/interp_body.c',
+                                         'numexpr/complex_functions.inc'],
+                             'extra_compile_args': ['-funroll-all-loops'],}
+    dict_append(extension_config_data, **mkl_config_data)
+    config.add_extension('interpreter',
+                         **extension_config_data)
+    
+    config.make_config_py()
+    config.add_data_files( ('', '*.txt'),
+                           #('', 'site.cfg.example'),
+                           #('numexpr', 'VERSION'),
+                           )
+    config.add_subpackage('tests', 'numexpr/tests')
+
+    #version handling
+    config.make_svn_version_py()    
+    config.get_version('numexpr/version.py')
+    return config
+
+def setup_package():
+    import os
+    from numpy.distutils.core import setup
 
 
-extra_setup_opts = {}
-if setuptools:
-    extra_setup_opts['zip_safe'] = False
+    setup(name='numexpr',
+          description='Fast numerical expression evaluator for NumPy',
+          author='David M. Cooke, Tim Hochberg, Francesc Alted, Ivan Vilata',
+          author_email='david.m.cooke@gmail.com, faltet@pytables.org',
+          url='http://code.google.com/p/numexpr/',
+          license = 'MIT',
+          packages = ['numexpr'],
+          configuration = configuration,
+          #**extra_setup_opts
+          )
 
-if use_vml:
-    intel_root = "/opt/intel/Compiler/11.0/074"   # XXX site-dependent
-    vml_include_dirs = [os.path.join(intel_root, 'mkl/include')]
-    vml_library_dirs = [os.path.join(intel_root, 'mkl/lib/em64t')]
-    vml_library_dirs += [os.path.join(intel_root, 'lib/intel64')]
-    vml_libraries = ['mkl_gf_lp64',
-                     'mkl_gnu_thread',
-                     'mkl_core',
-                     'iomp5',
-                     #'gfortran',  # XXX I don't seem to need it
-                     ]
-    vml_compile_args = ['-DUSE_VML']
-else:
-    vml_include_dirs = []
-    vml_library_dirs = []
-    vml_libraries = []
-    vml_compile_args = []
+#some old code I was not able to use with numpy.distutils
 
-interpreter_ext = Extension('numexpr.interpreter',
-                            sources=['numexpr/interpreter.c'],
-                            depends = ['numexpr/interp_body.c',
-                                       'numexpr/complex_functions.inc'],
-                            include_dirs=[numpy.get_include()] + \
-                                vml_include_dirs,
-                            library_dirs = vml_library_dirs,
-                            libraries = vml_libraries,
-                            extra_compile_args=['-funroll-all-loops'] + vml_compile_args
-                            )
+## setuptools should be able to find out by itself the right option for zip_save
+#     try:
+#         import setuptools
+#     except ImportError:
+#         setuptools = None
 
-class build_ext(old_build_ext):
-    def build_extension(self, ext):
-        # at this point we know what the C compiler is.
-        c = self.compiler
-        old_compile_options = None
-        if ext is interpreter_ext:
-            # For MS Visual C, we use /O1 instead of the default /Ox,
-            # as /Ox takes a long time (~20 mins) to compile.
-            # The speed of the code isn't noticeably different.
-            if c.compiler_type == 'msvc':
-                if not c.initialized:
-                    c.initialize()
-                old_compile_options = c.compile_options[:]
-                if '/Ox' in c.compile_options:
-                    c.compile_options.remove('/Ox')
-                c.compile_options.append('/O1')
-                ext.extra_compile_args = []
-        old_build_ext.build_extension(self, ext)
-        if old_compile_options is not None:
-            self.compiler.compile_options = old_compile_options
+#     extra_setup_opts = {}
+#     if setuptools:
+#         extra_setup_opts['zip_safe'] = False
 
-extra_setup_opts['cmdclass'] = {'build_ext': build_ext}
+## building on windows: who knows?
+# class build_ext(old_build_ext):
+#     def build_extension(self, ext):
+#         # at this point we know what the C compiler is.
+#         c = self.compiler
+#         old_compile_options = None
+#         if ext is interpreter_ext:
+#             # For MS Visual C, we use /O1 instead of the default /Ox,
+#             # as /Ox takes a long time (~20 mins) to compile.
+#             # The speed of the code isn't noticeably different.
+#             if c.compiler_type == 'msvc':
+#                 if not c.initialized:
+#                     c.initialize()
+#                 old_compile_options = c.compile_options[:]
+#                 if '/Ox' in c.compile_options:
+#                     c.compile_options.remove('/Ox')
+#                 c.compile_options.append('/O1')
+#                 ext.extra_compile_args = []
+#         old_build_ext.build_extension(self, ext)
+#         if old_compile_options is not None:
+#             self.compiler.compile_options = old_compile_options
 
-pkgname = 'numexpr'
-version = open(os.path.join(pkgname, 'VERSION')).read().strip()
+# extra_setup_opts['cmdclass'] = {'build_ext': build_ext}
 
-setup(name=pkgname,
-      version=version,
-      description='Fast numerical expression evaluator for NumPy',
-      author='David M. Cooke, Tim Hochberg, Francesc Alted, Ivan Vilata',
-      author_email='david.m.cooke@gmail.com, faltet@pytables.org',
-      url='http://code.google.com/p/numexpr/',
-      packages=[pkgname, pkgname+'.tests'],
-      package_data={pkgname: ['VERSION']},
-      ext_modules=[interpreter_ext],
-      **extra_setup_opts
-    )
+if __name__ == '__main__':
+    setup_package()
