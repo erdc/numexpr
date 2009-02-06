e41e9a4ff64e1b5604a5f28b8fcf2397a0e9e7c3
faltet <faltet@23631f54-0842-0410-b4f0-d76cd42f001b>
2009-02-06 16:32:20 +0000

    Fixed the personalization function for MSVC in setup.py.
    git-svn-id: http://numexpr.googlecode.com/svn/branches/vml_support@60 23631f54-0842-0410-b4f0-d76cd42f001b

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
+from numpy.distutils.command.build_ext import build_ext as numpy_build_ext
+
+try:
+    import setuptools
+except ImportError:
+    setuptools = None
+extra_setup_opts = {}
+if setuptools:
+    extra_setup_opts['zip_safe'] = False
 
 def configuration():
     from numpy.distutils.misc_util import Configuration, dict_append
     from numpy.distutils.system_info import system_info
 
     config = Configuration(package_name = 'numexpr')
-
+    
     #try to find configuration for MKL, either from environment or site.cfg
     mkl_config_data = config.get_info('mkl')
     #some version of MKL need to be linked with libgfortran, for this, use
@@ -40,53 +49,37 @@ def setup_package():
     import os
     from numpy.distutils.core import setup
 
-
+    extra_setup_opts['cmdclass'] = {'build_ext': build_ext}
     setup(name='numexpr',
           description='Fast numerical expression evaluator for NumPy',
-          author='David M. Cooke, Tim Hochberg, Francesc Alted, Ivan Vilata',
+          author='David M. Cooke and others',
           author_email='david.m.cooke@gmail.com, faltet@pytables.org',
           url='http://code.google.com/p/numexpr/',
           license = 'MIT',
           packages = ['numexpr'],
           configuration = configuration,
-          #**extra_setup_opts
+          **extra_setup_opts
           )
 
-#some old code I was not able to use with numpy.distutils
-
-## setuptools should be able to find out by itself the right option for zip_save
-#     try:
-#         import setuptools
-#     except ImportError:
-#         setuptools = None
-
-#     extra_setup_opts = {}
-#     if setuptools:
-#         extra_setup_opts['zip_safe'] = False
-
-## building on windows: who knows?
-# class build_ext(old_build_ext):
-#     def build_extension(self, ext):
-#         # at this point we know what the C compiler is.
-#         c = self.compiler
-#         old_compile_options = None
-#         if ext is interpreter_ext:
-#             # For MS Visual C, we use /O1 instead of the default /Ox,
-#             # as /Ox takes a long time (~20 mins) to compile.
-#             # The speed of the code isn't noticeably different.
-#             if c.compiler_type == 'msvc':
-#                 if not c.initialized:
-#                     c.initialize()
-#                 old_compile_options = c.compile_options[:]
-#                 if '/Ox' in c.compile_options:
-#                     c.compile_options.remove('/Ox')
-#                 c.compile_options.append('/O1')
-#                 ext.extra_compile_args = []
-#         old_build_ext.build_extension(self, ext)
-#         if old_compile_options is not None:
-#             self.compiler.compile_options = old_compile_options
-
-# extra_setup_opts['cmdclass'] = {'build_ext': build_ext}
-
+class build_ext(numpy_build_ext):
+    def build_extension(self, ext):
+        # at this point we know what the C compiler is.
+        c = self.compiler
+        old_compile_options = None
+        # For MS Visual C, we use /O1 instead of the default /Ox,
+        # as /Ox takes a long time (~5 mins) to compile.
+        # The speed of the code isn't noticeably different.
+        if c.compiler_type == 'msvc':
+            if not c.initialized:
+                c.initialize()
+            old_compile_options = c.compile_options[:]
+            if '/Ox' in c.compile_options:
+                c.compile_options.remove('/Ox')
+            c.compile_options.append('/O1')
+            ext.extra_compile_args = []
+        numpy_build_ext.build_extension(self, ext)
+        if old_compile_options is not None:
+            self.compiler.compile_options = old_compile_options
+    
 if __name__ == '__main__':
     setup_package()
