a4c6389b93e02d794a9ffbd4f0af710a2fa91d69
faltet <faltet@23631f54-0842-0410-b4f0-d76cd42f001b>
2009-02-06 09:18:29 +0000

    Increased the BLOCK_SIZE1 to 4096 when using VML.  This shows much better performance when doing multithreading than using the value 256.
    In the future, when/if numexpr would support multithreading for the
    general case (not only when using VML), we should revise these values.
    
    
    
    git-svn-id: http://numexpr.googlecode.com/svn/branches/vml_support@55 23631f54-0842-0410-b4f0-d76cd42f001b

--- numexpr/interpreter.c
+++ numexpr/interpreter.c
@@ -21,11 +21,20 @@
 #include "missing_posix_functions.inc"
 #endif
 
+#ifdef USE_VML
 /* The values below have been tuned for a nowadays Core2 processor */
 /* Note: with VML functions a larger block size (e.g. 4096) allows to make use
  * of the automatic multithreading capabilities of the VML library */
+#define BLOCK_SIZE1 4096
+#define BLOCK_SIZE2 32
+#else
+/* The values below have been tuned for a nowadays Core2 processor */
+/* Note: without VML available a smaller block size is best, specially
+ * for the strided and unaligned cases.  However, this may change
+ * when/if numexpr would support multithreading for the non-VML case. */
 #define BLOCK_SIZE1 256
 #define BLOCK_SIZE2 8
+#endif
 
 /* This file and interp_body should really be generated from a description of
    the opcodes -- there's too much repetition here for manually editing */
