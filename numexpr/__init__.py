e32320904ee64c26b4876909dea71505c257a01b
Gregor.Thalhammer <Gregor.Thalhammer@23631f54-0842-0410-b4f0-d76cd42f001b>
2009-02-09 11:48:19 +0000

    implemented set_vml_accurace_mode as wrapper for _set_vml_accuracy_mode with string arguments: 'high', 'low', 'fast'. Returns old settings. Doesn't change mode if called with None as argument. Added set_vml_num_threads.
    git-svn-id: http://numexpr.googlecode.com/svn/branches/vml_support@67 23631f54-0842-0410-b4f0-d76cd42f001b

--- numexpr/__init__.py
+++ numexpr/__init__.py
@@ -1,7 +1,7 @@
 from __config__ import show as show_config, get_info
 if get_info('mkl'):
     use_vml = True
-    from numexpr.interpreter import set_vml_accuracy_mode, set_num_threads
+    from numexpr.interpreter import _set_vml_accuracy_mode, _set_vml_num_threads
 else:
     use_vml = False
 
@@ -17,3 +17,30 @@ dirname = os.path.dirname(__file__)
 
 __version__ = version.version
 
+def set_vml_accuracy_mode(mode):
+    """
+    set the accuracy mode for VML operations
+
+    'high': high accuracy mode (HA), <1 least significant bit
+    'low': low accuracy mode (LA), typically 1-2 least significant bits
+    'fast': enhanced performance mode (EP)
+    None: mode settings are ignored
+
+    returns old accuracy settings
+    """
+    if use_vml:
+        acc_dict = {None: 0, 'low': 1, 'high': 2, 'fast': 3}
+        acc_reverse_dict = {1: 'low', 2: 'high', 3: 'fast'}
+        if mode not in acc_dict.keys():
+            raise ValueError("mode argument must be one of: None, 'high', 'low', 'fast'")
+        retval = _set_vml_accuracy_mode(acc_dict.get(mode, 0))
+        return acc_reverse_dict.get(retval)
+    else:
+        return None
+    
+def set_vml_num_threads(nthreads):
+    """
+    set maximum number of threads used for VML operations.
+    """
+    if use_vml:
+        _set_vml_num_threads(nthreads)
